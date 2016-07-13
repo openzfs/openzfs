8c6ff43818722e587acc5d823f96d96b7e1055b4
Matthew Ahrens <mahrens@delphix.com>
2016-07-13 16:55:29 -0700

    fix copyright

--- usr/src/lib/libdtrace/common/dt_parser.c
+++ usr/src/lib/libdtrace/common/dt_parser.c
@@ -21,8 +21,8 @@
 
 /*
  * Copyright (c) 2003, 2010, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2012, 2014 by Delphix. All rights reserved.
  * Copyright (c) 2013, Joyent Inc. All rights reserved.
+ * Copyright (c) 2012, 2014 by Delphix. All rights reserved.
  */
 
 /*
