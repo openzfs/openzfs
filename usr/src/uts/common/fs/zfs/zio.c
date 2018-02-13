cb4619d56d93e4187ca0c5a2f6f3e9067f3e9d65
Matthew Ahrens <mahrens@delphix.com>
2018-02-13 11:37:56 -0800

    DLPX-57225 Reads from indirect mirrors only find one copy
    Reviewed at: http://reviews.delphix.com/r/38104/

--- usr/src/uts/common/fs/zfs/zio.c
+++ usr/src/uts/common/fs/zfs/zio.c
@@ -564,6 +564,7 @@ zio_create(zio_t *pio, spa_t *spa, uint64_t txg, const blkptr_t *bp,
 
 	zio = kmem_cache_alloc(zio_cache, KM_SLEEP);
 	bzero(zio, sizeof (zio_t));
+	zio->io_mirror_child_hint = -1;
 
 	mutex_init(&zio->io_lock, NULL, MUTEX_DEFAULT, NULL);
 	cv_init(&zio->io_cv, NULL, CV_DEFAULT, NULL);
