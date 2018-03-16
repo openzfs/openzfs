0f6341557cb62089f4152c231ddf9712641c0d8d
Pascal666 <Pascal@rkfd.com>
2018-03-16 16:25:30 -0500

    Add comment so snap_stats doesn't get removed if l2arc check gets changed

--- usr/src/cmd/stat/arcstat/arcstat.pl
+++ usr/src/cmd/stat/arcstat/arcstat.pl
@@ -166,8 +166,11 @@ sub init {
 	detailed_usage() if $vflag;
 	@hdr = @xhdr if $xflag;		#reset headers to xhdr
 
-	# check if L2ARC exists
+	# we want to capture the stats here, so that we can use them to check
+	# if an L2ARC device exists; but more importantly, so that we print
+	# the stats since boot as the first line of output from main().
 	snap_stats();
+	
 	if (defined $cur{"l2_size"}) {
 		$l2exist = 1;
 	}
