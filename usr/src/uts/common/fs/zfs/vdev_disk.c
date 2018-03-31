51c1899887bda57a4f8331a185d47e6f879597bf
Andy Fiddaman <omnios@citrus-it.co.uk>
2018-03-31 22:58:14 +0000

    9428 ZFS pool import should ignore unparseable devids

--- usr/src/uts/common/fs/zfs/vdev_disk.c
+++ usr/src/uts/common/fs/zfs/vdev_disk.c
@@ -23,6 +23,7 @@
  * Copyright (c) 2012, 2016 by Delphix. All rights reserved.
  * Copyright 2016 Nexenta Systems, Inc.  All rights reserved.
  * Copyright (c) 2013 Joyent, Inc.  All rights reserved.
+ * Copyright 2018 OmniOS Community Edition (OmniOSce) Association.
  */
 
 #include <sys/zfs_context.h>
@@ -324,10 +325,10 @@ vdev_disk_open(vdev_t *vd, uint64_t *psize, uint64_t *max_psize,
 	if (vd->vdev_devid != NULL) {
 		if (ddi_devid_str_decode(vd->vdev_devid, &dvd->vd_devid,
 		    &dvd->vd_minor) != 0) {
-			vd->vdev_stat.vs_aux = VDEV_AUX_BAD_LABEL;
 			vdev_dbgmsg(vd, "vdev_disk_open: invalid "
 			    "vdev_devid '%s'", vd->vdev_devid);
-			return (SET_ERROR(EINVAL));
+			dvd->vd_devid = NULL;
+			dvd->vd_minor = NULL;
 		}
 	}
 
@@ -364,7 +365,7 @@ vdev_disk_open(vdev_t *vd, uint64_t *psize, uint64_t *max_psize,
 		/*
 		 * Compare the devid to the stored value.
 		 */
-		if (error == 0 && vd->vdev_devid != NULL &&
+		if (error == 0 && dvd->vd_devid != NULL &&
 		    ldi_get_devid(dvd->vd_lh, &devid) == 0) {
 			if (ddi_devid_compare(devid, dvd->vd_devid) != 0) {
 				error = SET_ERROR(EINVAL);
@@ -387,7 +388,7 @@ vdev_disk_open(vdev_t *vd, uint64_t *psize, uint64_t *max_psize,
 	 * If we were unable to open by path, or the devid check fails, open by
 	 * devid instead.
 	 */
-	if (error != 0 && vd->vdev_devid != NULL) {
+	if (error != 0 && dvd->vd_devid != NULL) {
 		error = ldi_open_by_devid(dvd->vd_devid, dvd->vd_minor,
 		    spa_mode(spa), kcred, &dvd->vd_lh, zfs_li);
 	}
@@ -399,7 +400,7 @@ vdev_disk_open(vdev_t *vd, uint64_t *psize, uint64_t *max_psize,
 	 * level vdev validation will prevent us from opening the wrong device.
 	 */
 	if (error) {
-		if (vd->vdev_devid != NULL)
+		if (dvd->vd_devid != NULL)
 			validate_devid = B_TRUE;
 
 		if (vd->vdev_physpath != NULL &&
